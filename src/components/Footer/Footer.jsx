17ad7d19e2fc458a7f8395046cf6f2c8ed6906f5
Karina Pryvarska <karinusha5@gmail.com>
2026-08-05 12:32:33 +0300

    Implemented footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,21 +1,88 @@
-import AppLink from '../AppLink/AppLink';
+import { HOME_SECTION_IDS } from '../../constants/sectionIds';
 
+import { contactInfoItems } from '../../configs/contactConfig';
+import { workingHours } from '../../configs/workingHoursConfig';
+
+import ArrowIcon from '../../assets/icons/ArrowIcon';
+import Button from '../Button/Button';
 import Logo from '../Logo/Logo';
 
 import './Footer.css';
 
-function Footer({
-    description = '',
-    socials = [],
-    columns = [],
-    legalLinks = [],
-}) {
+function Footer() {
     return (
         <footer className='footer'>
-            <div className='container footer-container'>
+            <div className='container'>
                 <div className='footer-columns'>
                     <div>
                         <Logo />
+
+                        <p className='footer-brand-description'>
+                            Проєктно-монтажне підприємство. Електромонтажні
+                            роботи, проєктування та технічне обслуговування
+                            енергетичних систем з 2014 року.
+                        </p>
+
+                        <Button
+                            to={`/#${HOME_SECTION_IDS.contact}`}
+                            icon={<ArrowIcon />}
+                            className='footer-brand-cta'
+                        >
+                            Залишити заявку
+                        </Button>
+                    </div>
+
+                    <div>
+                        <div className='footer-title'>Контакти</div>
+
+                        <div className='footer-contact-list'>
+                            {contactInfoItems.map(
+                                ({ id, icon: Icon, link, label, text }) => (
+                                    <div
+                                        key={id}
+                                        className='footer-contact-item'
+                                    >
+                                        <div className='footer-contact-icon'>
+                                            <Icon />
+                                        </div>
+
+                                        <div className='footer-contact-text'>
+                                            {link ? (
+                                                <a href={link}>{label}</a>
+                                            ) : (
+                                                <p>{text}</p>
+                                            )}
+                                        </div>
+                                    </div>
+                                )
+                            )}
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className='footer-title'>Графік роботи</div>
+
+                        <div className='footer-hours-list'>
+                            {workingHours.map(
+                                ({ id, day, time, isClosed }) => (
+                                    <div
+                                        key={id}
+                                        className='footer-hours-row'
+                                    >
+                                        <span className='footer-hours-day'>
+                                            {day}
+                                        </span>
+                                        <span
+                                            className={`footer-hours-time${
+                                                isClosed ? ' is-closed' : ''
+                                            }`}
+                                        >
+                                            {time}
+                                        </span>
+                                    </div>
+                                )
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ src/configs/workingHoursConfig.js
@@ -0,0 +1,13 @@
+export const workingHours = [
+    {
+        id: 'weekdays',
+        day: 'Пн – Пт',
+        time: '09:00 – 17:00',
+    },
+    {
+        id: 'weekend',
+        day: 'Сб – Нд',
+        time: 'Вихідні',
+        isClosed: true,
+    },
+];
